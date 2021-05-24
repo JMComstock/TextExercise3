9ecc83e352aba0d89829eac32d88ba8305371395
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-05-24 17:08:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextExercise3
+{
+    class Program
+    {
+        //Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00).
+        //A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise,
+        //display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a 24-hour time into the console (ex 19:00): ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invlaid Time");
+                return;
+            }
+
+            var components = input.Split(':');
+            if (components.Length != 2)
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+            try
+            {
+                var hour = Convert.ToInt32(components[0]);
+                var minute = Convert.ToInt32(components[1]);
+
+                if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+                    Console.WriteLine("Ok");
+                else
+                    Console.WriteLine("Invalid Time");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Time");
+            }
+        }
+    }
+}
